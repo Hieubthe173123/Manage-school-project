aea936a1d3a968af831676989b0cd590832ac24d
DESKTOP-1N38VET\admin <trunghieubui866@gmail.com>
2024-05-14 21:50:56 +0700

    Create project

---
+++ mange-school-be/mange-school-be/src/test/java/Springboot/manage/school/mangeschoolbe/MangeSchoolBeApplicationTests.java
@@ -0,0 +1,13 @@
+package Springboot.manage.school.mangeschoolbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangeSchoolBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
