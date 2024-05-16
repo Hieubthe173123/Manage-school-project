411714f69c8802008d2c4251b8f2d0cdf0189d3c
DESKTOP-1N38VET\admin <trunghieubui866@gmail.com>
2024-05-16 21:48:15 +0700

    Create entity

--- WebApplication1/src/java/DAO/DBContext.java
+++ WebApplication1/src/java/DAO/DBContext.java
@@ -4,10 +4,30 @@
  */
 package DAO;
 
-/**
- *
- * @author admin
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DBContext {
+      protected Connection connection;
+    public DBContext()
+    {
+        try{
+            String user="sa";
+            String pass="12345678";
+            String url="jdbc:sqlserver://localhost:1433;databaseName=SWP391c";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        }
+        catch(Exception ex)
+        {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
     
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+        System.out.println(db.connection);
+    }
 }
